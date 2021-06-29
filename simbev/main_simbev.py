3ae989ef0577ce2fb4c8e7aed95157080b677366
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2021-06-29 15:31:02 +0200

    minor changes

--- simbev/main_simbev.py
+++ simbev/main_simbev.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     num_threads = cfg.getint('sim_params', 'num_threads')
 
     # create directory for standing times data
-    directory = r"simbev_nep_2035_results/"
+    directory = r"/home/local/RL-INSTITUT/kilian.helfenbein/Daten_flexibel_02/simbev_results/calculations_for_anya/simbev_nep_2035_results/"
     directory = Path(directory)
 
     # result dir
