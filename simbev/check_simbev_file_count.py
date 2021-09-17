869a3b595a8f88a28bc7e9c15735f7b555ce36fa
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2021-09-17 19:02:22 +0200

    file count check

--- simbev/check_simbev_file_count.py
+++ simbev/check_simbev_file_count.py
@@ -7,7 +7,11 @@ simbev_dir = Path(r"/storage/open_bea_calculations_v2/simbev_data/default_multi_
 
 file_count = 0
 
-for sub_dir in os.listdir(simbev_dir):
+sub_dirs = os.listdir(simbev_dir)
+
+print(len(sub_dirs))
+
+for sub_dir in sub_dirs:
     file_count += len(os.listdir(os.path.join(simbev_dir, sub_dir)))
 
 print(file_count)
